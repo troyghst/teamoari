5258c523d9f59a860bb7ebbb52966532f52cc248
troyghst <troykbrn@gmail.com>
2025-05-21 19:34:21 -0500

    Update sorpresa.js

--- sorpresa.js
+++ sorpresa.js
@@ -22,10 +22,11 @@ setTimeout(() => {
     questionElement.style.display = "block";
     buttonGroup.style.display = "block";
     typeQuestion();
-}, 5000); // Espera 5 segundos antes de mostrar la pregunta
+}, 15000); // Espera 5 segundos antes de mostrar la pregunta
 
 yesBtn.addEventListener("click", () => {
     responseMessage.textContent = "Eres el amor de mi vidaaaa y aunque estemos separados no dejare de amarte como el primer día, te amo demasiado mi ari preciosaaa💗💗💗";
+    responseMessage.textContent = "Si t tengo un regalito tmb, es d roblox : asd";
     responseMessage.style.display = "block";
     noBtn.style.display = "none";
     yesBtn.style.transform = "scale(1.2)";
